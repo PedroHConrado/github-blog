22cbc14fb5c872719261cf6e28106aed94fac6df
Pedro Conrado <conradohpedro@gmail.com>
2023-08-19 18:36:57 -0300

    add AmountPublication in Home page

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,10 +1,16 @@
 import { Profile } from "../../components/Profile";
-import { HomeContainer } from "./styles";
+import { Search } from "../../components/Search";
+import { Amount, AmountPublications, HomeContainer, Title } from "./styles";
 
 export function Home() {
   return (
     <HomeContainer> 
       <Profile />
+      <AmountPublications>
+        <Title>Publicações</Title>
+        <Amount>6 publicações</Amount>
+      </AmountPublications>
+      <Search />
     </HomeContainer>
   )
 }
--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -5,3 +5,29 @@ export const HomeContainer = styled.div`
   flex-direction: column;
   align-items: center;
 `
+
+export const AmountPublications = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+
+  margin-top: 4.5rem;
+  margin-bottom: 0.75rem;
+`
+
+export const Amount = styled.span`
+  color: ${props => props.theme["base-span"]};
+  text-align: right;
+
+  font-size: 14px;
+  line-height: 160%;
+`
+
+export const Title = styled.span`
+  color: ${props => props.theme["base-subtitle"]};
+
+  font-size: 18px;
+  font-weight: 700;
+  line-height: 160%; 
+`
