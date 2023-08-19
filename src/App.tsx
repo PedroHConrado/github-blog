f2bf1c6026ee13488d014a541f05f29214081092
Pedro <conradohpedro@gmail.com>
2023-08-19 15:01:46 -0300

    create project

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import { ThemeProvider } from "styled-components"
+import { GlobalStyle } from "./styles/global"
+import { defaultTheme } from "./styles/theme/default"
+import { Router } from './Router'
+import { BrowserRouter } from "react-router-dom"
+
+function App() {
+
+  return (
+    <ThemeProvider theme={defaultTheme}>
+      <GlobalStyle />
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
+    </ThemeProvider>
+  )
+}
+
+export default App
