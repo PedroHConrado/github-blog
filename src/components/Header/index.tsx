66ffb0b6aabc1c4fbfaa8fdcc0405010d40c990b
Pedro <conradohpedro@gmail.com>
2023-08-19 15:03:05 -0300

    create Header component

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,10 @@
+import { HeaderContainer, Logo } from "./styles";
+import logoImg from '../../assets/Logo.svg'
+
+export function Header() {
+  return (
+    <HeaderContainer>
+      <Logo src={logoImg}/>
+    </HeaderContainer>
+  )
+}
---
+++ src/components/Header/styles.ts
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+import imgHeader from '../../assets/Cover.png'
+
+export const HeaderContainer = styled.header`
+  background: url(${imgHeader}) no-repeat center;
+  width: 90rem;
+  height: 18.5rem;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`
+export const Logo = styled.img`
+  width: 148px;
+  height: 98px;
+`
