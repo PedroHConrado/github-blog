658371e5498aea8c35df9436a83c5e32b0995631
Pedro Conrado <conradohpedro@gmail.com>
2023-08-20 17:48:31 -0300

    populate Publication component

--- src/components/Publication/index.tsx
+++ src/components/Publication/index.tsx
@@ -1,9 +1,17 @@
-import { PublicationContainer } from "./styles";
+import { CreatedAt, Description, PublicationContainer, Title } from './styles'
 
 export function Publication() {
   return (
     <PublicationContainer>
-      
+      <div>
+        <Title>JavaScript data types and data structures</Title>
+        <CreatedAt>há 1 dia</CreatedAt>
+      </div>
+      <Description>
+        Programming languages all have built-in data structures, but these often
+        differ from one language to another. This article attempts to list the
+        built-in data structures available in
+      </Description>
     </PublicationContainer>
   )
 }
--- src/components/Publication/styles.ts
+++ src/components/Publication/styles.ts
@@ -1,8 +1,42 @@
-import { styled } from "styled-components";
+import { styled } from 'styled-components'
 
 export const PublicationContainer = styled.div`
   width: 26rem;
   height: 16.25rem;
 
-  background: ${props => props.theme["base-post"]};
+  display: flex;
+  flex-direction: column;
+  gap: 1.25rem;
+
+  background: ${(props) => props.theme['base-post']};
+  border-radius: 10px;
+  padding: 2rem;
+
+  div {
+    display: flex;
+  }
+`
+export const Title = styled.span`
+  width: 283px;
+  color: ${(props) => props.theme['base-title']};
+
+  font-size: 20px;
+  font-weight: 700;
+  line-height: 160%;
+`
+export const Description = styled.span`
+  overflow: hidden;
+  color: ${(props) => props.theme['base-text']};
+  text-overflow: ellipsis;
+
+  font-size: 16px;
+  font-style: normal;
+  font-weight: 400;
+  line-height: 160%;
+`
+export const CreatedAt = styled.span`
+  color: ${(props) => props.theme['base-span']};
+
+  font-size: 14px;
+  line-height: 160%;
 `
