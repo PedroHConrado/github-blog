38d60f95fdbdbb056277d67e2fd4821551bd2f28
Pedro Conrado <conradohpedro@gmail.com>
2023-08-20 17:50:38 -0300

    create PostContent component

---
+++ src/pages/Post/components/PostContent/styles.ts
@@ -0,0 +1,10 @@
+import styled from 'styled-components'
+
+export const PostContentContainer = styled.div`
+  padding: 2.5rem 2rem;
+  width: 100%;
+
+  color: ${(props) => props.theme['base-text']};
+  font-size: 16px;
+  line-height: 160%;
+`
