13ce2956e32c077b49942db11bac9d27ea8c5aad
Pedro Conrado <conradohpedro@gmail.com>
2023-08-20 17:49:20 -0300

    add ListPublication in Home page

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,16 +1,29 @@
-import { Profile } from "../../components/Profile";
-import { Search } from "../../components/Search";
-import { Amount, AmountPublications, HomeContainer, Title } from "./styles";
+import { Profile } from '../../components/Profile'
+import { Publication } from '../../components/Publication'
+import { Search } from '../../components/Search'
+import {
+  Amount,
+  AmountPublications,
+  HomeContainer,
+  ListPublication,
+  Title,
+} from './styles'
 
 export function Home() {
   return (
-    <HomeContainer> 
+    <HomeContainer>
       <Profile />
       <AmountPublications>
         <Title>Publicações</Title>
         <Amount>6 publicações</Amount>
       </AmountPublications>
       <Search />
+      <ListPublication>
+        <Publication />
+        <Publication />
+        <Publication />
+        <Publication />
+      </ListPublication>
     </HomeContainer>
   )
 }
--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -1,11 +1,12 @@
-import { styled } from "styled-components";
+import { styled } from 'styled-components'
 
 export const HomeContainer = styled.div`
+  width: 54rem;
   display: flex;
   flex-direction: column;
   align-items: center;
+  justify-content: center;
 `
-
 export const AmountPublications = styled.div`
   width: 100%;
   display: flex;
@@ -15,19 +16,25 @@ export const AmountPublications = styled.div`
   margin-top: 4.5rem;
   margin-bottom: 0.75rem;
 `
-
 export const Amount = styled.span`
-  color: ${props => props.theme["base-span"]};
+  color: ${(props) => props.theme['base-span']};
   text-align: right;
 
   font-size: 14px;
   line-height: 160%;
 `
-
 export const Title = styled.span`
-  color: ${props => props.theme["base-subtitle"]};
+  color: ${(props) => props.theme['base-subtitle']};
 
   font-size: 18px;
   font-weight: 700;
-  line-height: 160%; 
+  line-height: 160%;
+`
+export const ListPublication = styled.div`
+  display: flex;
+  gap: 2rem;
+  flex-wrap: wrap;
+  justify-content: center;
+
+  margin-top: 48px;
 `
